d8523068e7bd554b2af2bff87fe45aa49ab4b033
calebellishutchings <calebellishutchings@cedarville.edu>
2026-05-10 16:33:56 -0400

    primary scene templated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "astro/entrypoints/prerender": fileURLToPath(
+          new URL("./node_modules/astro/dist/entrypoints/prerender.js", import.meta.url)
+        ),
+        "astro/runtime/client/dev-toolbar/entrypoint.js": fileURLToPath(
+          new URL(
+            "./node_modules/astro/dist/runtime/client/dev-toolbar/entrypoint.js",
+            import.meta.url
+          )
+        )
+      }
+    }
+  }
+});
